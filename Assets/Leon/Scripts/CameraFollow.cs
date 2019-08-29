ccb6e3654ae7f72150c21e245677b2447302c2a6
Leon Becker <lab160730@utdallas.edu>
2019-08-28 19:54:39 -0500

    movement and bad camera follow scripts

---
+++ Assets/Leon/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 offset = new Vector3(0.0f,10.0f,-10.0f);
+    // Start is called before the first frame update
+    void Start(){
+        gameObject.transform.rotation = new Quaternion(0.0f,0.0f,0.0f,1.0f);
+        gameObject.transform.Rotate(new Vector3(1.0f,0.0f,0.0f),53.13f);
+        gameObject.transform.position = player.transform.position+offset;
+    }
+    // Update is called once per frame
+    void Update(){
+        gameObject.transform.position = player.transform.position +offset;
+    }
+}
