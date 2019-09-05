812137bc59a1dfcb66cca9da12e944a2b3957429
rodgers-matt <mattr2013@outlook.com>
2019-09-04 20:07:57 -0500

    Updated gun scripts

--- Assets/Matt/Scripts/Weapons/RapidFire.cs
+++ Assets/Matt/Scripts/Weapons/RapidFire.cs
@@ -8,12 +8,14 @@ public class RapidFire : MonoBehaviour
     public MachineGunBullet bullet;
     public Transform gun;
     public GameObject crossHair;
-    public float delay = .04f;
+    public float delay = .5f;
+    private float shootDelay = 0.0f;
+    public int ammo = 50;
 
     // Update is called once per frame
     private void Start()
     {
-        
+
     }
     void Update()
     {
@@ -23,13 +25,12 @@ public class RapidFire : MonoBehaviour
         else if(Input.GetButtonUp("Fire1"))
             triggerDown = false;
         //If the user is firing
-        if (triggerDown)
+        if (triggerDown && Time.time > shootDelay && ammo > 0)
         {
             MachineGunBullet newBullet = Instantiate(bullet, gun.position, gun.rotation) as MachineGunBullet;
             newBullet.crossHair = crossHair;
+            shootDelay = Time.time + delay;
+            ammo--;
         }
     }
-
-    
-
 }
