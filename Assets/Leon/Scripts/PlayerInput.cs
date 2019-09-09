f42448213517ef9c4315bda1fca2a180286aae79
Leon Becker <lab160730@utdallas.edu>
2019-09-09 15:44:22 -0500

    Added the PlayerInput script, so far it's really bad and I may need to rethink it. Player also looks the direction of the mouse now, but its slightly off

---
+++ Assets/Leon/Scripts/PlayerInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (Player))]
+public class PlayerInput : MonoBehaviour
+{
+    Player player;
+    [SerializeField]
+    Camera cam;
+
+    void Start()
+    {
+        player = GetComponent<Player>();
+        player.SetPlayerCoords2D(cam.WorldToScreenPoint(player.transform.position));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 directionalInput = new Vector3(Input.GetAxis("Horizontal"),0.0f,Input.GetAxis("Vertical"));
+        player.SetMoveDirection(directionalInput);
+
+        player.SetLookDirection(Input.mousePosition);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            player.Jump();
+        }
+
+    }
+
+}
